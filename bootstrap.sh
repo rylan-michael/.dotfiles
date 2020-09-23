33590cbebca41eae5c1abaaa3749466e7faef4cb
Rylan McCarty <rylanmmccarty@gmail.com>
2020-09-23 15:21:44 -0500

    Remove configuration for Alacritty after the move back to iTerm2

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,6 @@
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 ln -s ${BASEDIR}/vimrc ~/.vimrc
-ln -s ${BASEDIR}/.alacritty.yml ~/.alacritty.yml
 ln -s ${BASEDIR}/.oh-my-zsh ~/.oh-my-zsh
 ln -s ${BASEDIR}/.zshrc ~/.zshrc
 ln -s ${BASEDIR}/.emacs.d ~/.emacs.d
