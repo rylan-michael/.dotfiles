ba885033eeda6d9c80a09b78fbfb656dcce911a3
Rylan McCarty <rylanmmccarty@gmail.com>
2020-09-23 15:55:14 -0500

    Edit bootstrap.sh to reflect naming changes

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,5 @@
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 ln -s ${BASEDIR}/vimrc ~/.vimrc
-ln -s ${BASEDIR}/.oh-my-zsh ~/.oh-my-zsh
-ln -s ${BASEDIR}/.zshrc ~/.zshrc
-ln -s ${BASEDIR}/.emacs.d ~/.emacs.d
-ln -s ${BASEDIR}/.spacemacs ~/.spacemacs
+ln -s ${BASEDIR}/oh-my-zsh ~/.oh-my-zsh
+ln -s ${BASEDIR}/zshrc ~/.zshrc
