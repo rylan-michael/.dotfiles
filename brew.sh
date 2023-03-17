be65324be41cd0f84ccfa1ea9c468354d28f4738
Rylan McCarty <rylanmmccarty@gmail.com>
2023-03-16 22:52:15 -0500

    Add nvim brew dependency

--- brew.sh
+++ brew.sh
@@ -23,21 +23,55 @@ BREW_PREFIX=$(brew --prefix)
 # Install GNU `sed`, overwriting the built-in `sed`.
 #brew install gnu-sed --with-default-names
 
-# Install personal macOS tools.
-brew install ripgrep
-brew install php
-
 # Install other useful binaries.
-brew install lua
 brew install tree
+brew install pandoc
+brew install ngrep
+brew install ripgrep
+
+# TODO(rylan): Determine whether to transition to container-based dev environments.
+# Install development utilities.
+brew install openjdk@11
+brew install k6 # Load testing https://k6.io
+brew install python@3.10
+brew install poetry # Python package management.
+brew install rustup-init
+
+# Install formula
+brew install --formula \
+nvim
 
 # Install applications.
-brew cask install hammerspoon
-brew cask install rectangle
-brew cask install iterm2
-brew cask install sublime-merge
-brew cask install font-firacode-nerd-font
-brew cask install bitwarden
+brew install --cask \
+calibre \
+beekeeper-studio \
+bitwarden \
+blender \
+docker \
+firefox \
+font-firacode-nerd-font \
+google-chrome \
+hot \
+iterm2 \
+microsoft-edge \
+ngrok \
+notion \
+obsidian \
+oracle-jdk \
+plex-media-server \
+postman \
+rectangle \
+sequel-pro \
+spotify \
+steam \
+sublime-merge \
+sqlite \
+visual-studio-code \
+vlc \
+wireshark \
+zotero
+# obs \
+# virtualbox \
 
 # Remove outdated versions from the cellar.
 brew cleanup
