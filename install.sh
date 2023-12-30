176d0ff0f4405c26850b71d9b17163520107fbfd
Rylan McCarty <rylan@protopacer.com>
2023-12-30 14:48:05 -0600

    Fix array parsing bug in install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env zsh
-
 if [[ -z $STOW_FOLDERS ]]; then
-    STOW_FOLDERS="zsh"
+    STOW_FOLDERS="zsh,nvim"
 fi
 
 if [[ -z $DOTFILES ]]; then
@@ -9,10 +8,15 @@ if [[ -z $DOTFILES ]]; then
 fi
 
 pushd "$DOTFILES" || { echo "Failed to change directory to $DOTFILES"; exit 1; }
-for folder in ${STOW_FOLDERS//,/ /}
+IFS=',' read -A folders <<< "$STOW_FOLDERS"
+for folder in "${folders[@]}"
 do
-    echo "stow $folder"
+    echo "stow: DEBUG: configuring package '$folder'"
     stow -D "$folder"
     stow "$folder"
 done
 popd || { echo "popd failed"; exit 1; }
+
+# `<<<` operator to pass the contents of the `STOW_FOLDERS` environment variable to the `read` command.
+# We set the `IFS` (Internal Field Separator) to ',' to specify comma-separate values.
+# The `-A` flag is used with `read` to read the input as an array and store the result in the `folders` variable.
