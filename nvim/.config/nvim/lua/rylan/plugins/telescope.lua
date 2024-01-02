558be3dac6180da1a0197cf6a884f48210382ba0
Rylan McCarty <rylan@protopacer.com>
2024-01-01 23:05:46 -0600

    nvim: Change telescope mappings

--- nvim/.config/nvim/lua/rylan/plugins/telescope.lua
+++ nvim/.config/nvim/lua/rylan/plugins/telescope.lua
@@ -4,13 +4,11 @@ return {
     config = function()
         local builtin = require('telescope.builtin')
 
-        -- Project search for Git-tracked files
-        vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
-
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-        vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-        vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+        vim.keymap.set('n', '<leader>pg', builtin.git_files, { desc = 'Project search for Git-tracked files' })
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find files' })
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Find files using live grep search' })
+        vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Find buffers' })
+        vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Find help' })
 
         -- Custom keybinding for searching through dotfiles
         -- Find more references: https://github.com/tjdevries/config_manager/blob/ee11710c4ad09e0b303e5030b37c86ad8674f8b2/xdg_config/nvim/lua/tj/telescope/init.lua
@@ -25,6 +23,6 @@ return {
                     height = 0.45
                 }
             })
-        end, {})
+        end, { desc = 'Find in dotfiles' })
     end
 }
