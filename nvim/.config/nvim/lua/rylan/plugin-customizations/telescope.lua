37c68d500c4d1a81ab0620ddd48c70136c2d4692
Rylan McCarty <rylan@protopacer.com>
2024-01-09 00:41:41 -0600

    nvim: live_grep should include search over hidden files

--- nvim/.config/nvim/lua/rylan/plugin-customizations/telescope.lua
+++ nvim/.config/nvim/lua/rylan/plugin-customizations/telescope.lua
@@ -1,5 +1,44 @@
 local M = {}
 
+-- Function to find the git root directory based on the current buffer's path
+function M.find_git_root()
+	local current_file = vim.api.nvim_buf_get_name(0)
+	local current_dir
+	local cwd = vim.fn.getcwd()
+	-- If the buffer is not associate with a file, return nil
+	if current_file == "" then
+		current_dir = cwd
+	else
+		-- Extract the directory from the current file's path
+		current_dir = vim.fn.fnamemodify(current_file, ":h")
+	end
+
+	-- Find the Git root directory from the current file's path
+	local git_root = vim.fn.systemlist("git -C " .. vim.fn.escape(current_dir, " ") .. " rev-parse --show-toplevel")[1]
+	if vim.v.shell_error ~= 0 then
+		print("Not a git repository. Searching on current working directory")
+		return cwd
+	end
+	return git_root
+end
+
+function M.live_grep_git_root()
+	print("called live_grep_git_root")
+	local git_root = M.find_git_root()
+	print(git_root)
+	if git_root then
+		print("success")
+		require("telescope.builtin").live_grep({
+			search_dirs = { git_root },
+			additional_args = function(opts)
+				return {
+					"--hidden",
+				}
+			end,
+		})
+	end
+end
+
 function M.search_all_files()
 	require("telescope.builtin").find_files({
 		find_command = { "rg", "--files" },
@@ -41,13 +80,21 @@ return {
 	config = function()
 		local builtin = require("telescope.builtin")
 
+		-- Enable telescope fzf native, if installed
+		pcall(require("telescope").load_extension, "fzf")
+
 		vim.keymap.set(
 			"n",
 			"<leader>pg",
 			builtin.git_files,
 			{ desc = "Telescope: Project search for Git-tracked files" }
 		)
-		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope: Find files using live grep search" })
+		vim.keymap.set(
+			"n",
+			"<leader>fg",
+			M.live_grep_git_root,
+			{ desc = "Telescope: Find files using live grep search" }
+		)
 		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope: Find buffers" })
 		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope: Find help" })
 
